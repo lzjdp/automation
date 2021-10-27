ffe270ec456f5ebe9c8652b6785d3ef9d193d326
carrlu <carrlu@163.com>
2021-10-27 10:07:03 +0800

    util

--- src/utils/util.js
+++ src/utils/util.js
@@ -3,3 +3,7 @@ const tokenKey = 'admin-key'
 export function getToken () {
   return window.localStorage.getItem(tokenKey)
 }
+
+export function setToken (token) {
+  return window.localStorage.setItem(tokenKey, token)
+}
