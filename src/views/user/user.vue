8c8010bc69f13f703f31e5af4c44739bc5affc0e
carrlu <carrlu@163.com>
2021-10-28 09:21:41 +0800

    yml

--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -1,13 +1,13 @@
 <!--
  * @Author: your name
  * @Date: 2021-10-27 18:21:25
- * @LastEditTime: 2021-10-28 09:18:30
+ * @LastEditTime: 2021-10-28 09:21:37
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /automation/src/views/user/user.vue
 -->
 <template>
-  <div class="user">user</div>
+  <div class="user"></div>
 </template>
 
 <script>
