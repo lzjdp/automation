ecfb2effb9f0b85d15c80bfc401c8b509a4e2a0f
carrlu <carrlu@163.com>
2021-10-28 08:59:37 +0800

    yml

--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-10-27 18:21:25
+ * @LastEditTime: 2021-10-28 08:59:33
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /automation/src/views/user/user.vue
+-->
 <template>
   <div class="user"></div>
 </template>
@@ -6,8 +14,6 @@
 export default {
   data () {
     return {}
-  },
-  mounted () {},
-  methods: {}
+  }
 }
 </script>
