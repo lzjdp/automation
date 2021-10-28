f96ae7011d9d57ddbae1425cda4c840642ace9e2
carrlu <carrlu@163.com>
2021-10-28 10:11:29 +0800

    yml

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,5 +1,13 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-10-26 12:01:20
+ * @LastEditTime: 2021-10-28 10:11:24
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /automation/src/views/home/home.vue
+-->
 <template>
-  <div class="home">home</div>
+  <div class="home"></div>
 </template>
 
 <script>
