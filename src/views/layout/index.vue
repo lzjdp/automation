7d47cc5bfd74ec6c6b916532f0d814a3cb569a53
carrlu <carrlu@163.com>
2021-10-28 08:58:38 +0800

    yml

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-10-27 10:43:21
+ * @LastEditTime: 2021-10-28 08:58:18
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /automation/src/views/layout/index.vue
+-->
 <template>
   <div class="layout">layout</div>
 </template>
@@ -12,10 +20,6 @@ export default {
     return {
 
     }
-  },
-  mounted () {},
-  methods: {
-    get () {}
   }
 }
 </script>
